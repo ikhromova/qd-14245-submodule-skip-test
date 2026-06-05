fcc071cdca8cbaef557b9694711f026262d735d6
ikhromova <ikhromova@github>
2026-06-05 11:42:51 +0300

    feat: add greeting, update submodule URL (this commit has broken submodule URL)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Added feature: greeting with submodule
+        System.out.println("Hello World from feature branch");
     }
 }
